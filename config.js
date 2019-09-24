cfcac122937e55beaa5c7916106c2b5d1e6de902
Piotr Fidler <fidler.piotr@gmail.com>
2019-09-24 21:14:10 +0200

    true google auth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '357089160265-381gtii0167of7k9sdq79svlo6n1j7v0.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '_FbnhSCwIb3LRJpSewBLZuUI',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+  };
--- server.js
+++ server.js
@@ -1,29 +1,57 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', function(req, res) {
-    res.send('Logowanie do Google localhost:3000/auth/google');
+app.get('/', function(req, res){
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', function(req, res){
-    res.render('google_auth');
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
 
-app.get('/auth/google_logged_in', function(req, res){
-    res.render('google_logged_in');
-});
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
-
-var server = app.listen(3000, "localhost", function() {
-    var host = server.address().address;
-    var port = server.address().port;
-  
-    console.log("Przykładowa aplikacja nasłuchuje na http://" + host + ":" + port);
-  });
-  
-  app.use(function(req, res, next) {
-    res.status(404).send("Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!");
-  });
+    var server = app.listen(3000, "localhost", function() {
+        var host = server.address().address;
+        var port = server.address().port;
+      
+        console.log("Autoryzacja Google nasłuchuje na http://" + host + ":" + port);
+      });
